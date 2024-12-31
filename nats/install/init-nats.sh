10b8e06f998a849e0dac82891149504adb6f17b8
IDCT Bartosz Pachołek <bartosz+github@idct.tech>
2024-12-31 01:15:56 +0100

    separate nats inits

--- nats/install/init-nats.sh
+++ nats/install/init-nats.sh
@@ -2,6 +2,6 @@
 nats --server=nats://localhost:4222 --user=admin --password="password" stream add high --retention=limits --discard=old --storage=file --subjects="high" --replicas=1 --max-age="30m" --defaults
 nats --server=nats://localhost:4222 --user=admin --password="password" stream add low --retention=limits --discard=old --storage=file --subjects="low" --replicas=1 --max-age="30m" --defaults
 nats --server=nats://localhost:4222 --user=admin --password="password" stream add normal --retention=limits --discard=old --storage=file --subjects="normal" --replicas=1 --max-age="30m" --defaults
-nats --server=nats://localhost:4222 --user=admin --password="password" consumer add high client --ack=explicit --deliver=all --deliver-group=main --pull --defaults
-nats --server=nats://localhost:4222 --user=admin --password="password" consumer add low client --ack=explicit --deliver=all --deliver-group=main --pull --defaults
-nats --server=nats://localhost:4222 --user=admin --password="password" consumer add normal client --ack=explicit --deliver=all --deliver-group=main --pull --defaults
+nats --server=nats://localhost:4222 --user=admin --password="password" consumer add high client --ack=explicit --deliver=all --deliver-group=high1 --pull --defaults
+nats --server=nats://localhost:4222 --user=admin --password="password" consumer add low client --ack=explicit --deliver=all --deliver-group=low1 --pull --defaults
+nats --server=nats://localhost:4222 --user=admin --password="password" consumer add normal client --ack=explicit --deliver=all --deliver-group=normal1 --pull --defaults

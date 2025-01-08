23da95f4c114ae1edc854b35d9e35b26c7519084
IDCT Bartosz Pachołek <bartosz+github@idct.tech>
2025-01-08 15:37:23 +0100

    new stream

--- nats/install/init-nats.sh
+++ nats/install/init-nats.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-nats --server=nats://localhost:4222 --user=admin --password="password" stream add high --retention=limits --discard=old --storage=file --subjects="high" --replicas=1 --max-age="30m" --defaults
+nats --server=nats://localhost:4222 --user=admin --password="password" stream add prematch --retention=limits --discard=old --storage=file --subjects="prematch" --replicas=1 --max-age="30m" --defaults
+nats --server=nats://localhost:4222 --user=admin --password="password" stream add inplay --retention=limits --discard=old --storage=file --subjects="inplay" --replicas=1 --max-age="30m" --defaults
 nats --server=nats://localhost:4222 --user=admin --password="password" stream add low --retention=limits --discard=old --storage=file --subjects="low" --replicas=1 --max-age="30m" --defaults
 nats --server=nats://localhost:4222 --user=admin --password="password" stream add normal --retention=limits --discard=old --storage=file --subjects="normal" --replicas=1 --max-age="30m" --defaults
-nats --server=nats://localhost:4222 --user=admin --password="password" consumer add high client --ack=explicit --deliver=all --deliver-group=high1 --pull --defaults
+nats --server=nats://localhost:4222 --user=admin --password="password" consumer add prematch client --ack=explicit --deliver=all --deliver-group=prematch1 --pull --defaults
+nats --server=nats://localhost:4222 --user=admin --password="password" consumer add inplay client --ack=explicit --deliver=all --deliver-group=inplay1 --pull --defaults
 nats --server=nats://localhost:4222 --user=admin --password="password" consumer add low client --ack=explicit --deliver=all --deliver-group=low1 --pull --defaults
 nats --server=nats://localhost:4222 --user=admin --password="password" consumer add normal client --ack=explicit --deliver=all --deliver-group=normal1 --pull --defaults
